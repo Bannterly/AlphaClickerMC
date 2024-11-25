db28aedb09d4d1f98775d1cc9c9f33a6bd19d72a
Bannterly <111296356+Bannterly@users.noreply.github.com>
2024-11-25 00:45:09 +0000

    config added  WinApi.cs

--- Alphaclicker/WinApi.cs
+++ Alphaclicker/WinApi.cs
@@ -18,6 +18,18 @@ namespace AlphaClicker
             }
         }
 
+        private enum MOUSEEVENTF
+        {
+            LEFTDOWN = 0x0002,
+            LEFTUP = 0x0004,
+            MIDDLEDOWN = 0x0020,
+            MIDDLEUP = 0x0040,
+            RIGHTDOWN = 0x0008,
+            RIGHTUP = 0x0010,
+            MOVE = 0x0001,
+            ABSOLUTE = 0x8000
+        }
+
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
 
@@ -39,17 +51,29 @@ namespace AlphaClicker
 
         public static void DoClick(string button, bool useCustomCoords, int X, int Y)
         {
-            if (useCustomCoords) { SetCursorPos(X, Y); }
+            if (useCustomCoords)
+            {
+                SetCursorPos(X, Y);
+            }
+            else
+            {
+                // Get current cursor position if not using custom coordinates
+                POINT currentPos;
+                GetCursorPos(out currentPos);
+                X = currentPos.X;
+                Y = currentPos.Y;
+            }
+
             switch (button)
             {
                 case "Left":
-                    mouse_event((uint)MOUSEEVENTF.LEFTDOWN | (uint)MOUSEEVENTF.LEFTUP, 0, 0, 0, 0);
+                    mouse_event((uint)MOUSEEVENTF.LEFTDOWN | (uint)MOUSEEVENTF.LEFTUP, (uint)X, (uint)Y, 0, 0);
                     break;
                 case "Right":
-                    mouse_event((uint)MOUSEEVENTF.RIGHTDOWN | (uint)MOUSEEVENTF.RIGHTUP, 0, 0, 0, 0);
+                    mouse_event((uint)MOUSEEVENTF.RIGHTDOWN | (uint)MOUSEEVENTF.RIGHTUP, (uint)X, (uint)Y, 0, 0);
                     break;
                 case "Middle":
-                    mouse_event((uint)MOUSEEVENTF.MIDDLEDOWN | (uint)MOUSEEVENTF.MIDDLEUP, 0, 0, 0, 0);
+                    mouse_event((uint)MOUSEEVENTF.MIDDLEDOWN | (uint)MOUSEEVENTF.MIDDLEUP, (uint)X, (uint)Y, 0, 0);
                     break;
             }
         }
